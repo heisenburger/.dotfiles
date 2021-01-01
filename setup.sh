42876b2103bf50c003f6850330bf0d21d22c2979
heisenburger <serenadantingchen@gmail.com>
2021-01-01 19:07:32 +0100

    add kitty config

--- setup.sh
+++ setup.sh
@@ -6,14 +6,17 @@ for f in .bashrc \
          .bash_profile \
          .gitconfig \
          .gitignore_global \
-         .git-completion.bash \ 
+         .git-completion.bash \
          .hushlogin \
          .vimr \
-         zprofile \
-         zshrcc
+         .zprofile \
+         .zshrc
 do
   printf '\n'
   echo "$CURRENT_DIR/$f -> $HOME/$f"
   ln -s "$CURRENT_DIR/$f" "$HOME/$f"
 done
+
+echo "$CURRENT_DIR/config/kitty.conf -> $HOME/.config/kitty/kitty.conf"
+ln -is "$CURRENT_DIR/config/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
