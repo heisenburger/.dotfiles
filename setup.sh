c0d68ad4c421b92aca7050a710a178a302ce77b0
heisenburger <serenadantingchen@gmail.com>
2025-01-12 18:33:12 -0500

    ssh config

--- setup.sh
+++ setup.sh
@@ -16,6 +16,10 @@ do
   ln -s "$CURRENT_DIR/$f" "$HOME/$f"
 done
 
+# the ssh config is in a different place
+echo "$CURRENT_DIR/.ssh-config -> $HOME/.ssh/config"
+ln -s "$CURRENT_DIR/.ssh-config" "$HOME/.ssh/config"
+
 echo "$CURRENT_DIR/config/kitty.conf -> $HOME/.config/kitty/kitty.conf"
 ln -is "$CURRENT_DIR/config/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
